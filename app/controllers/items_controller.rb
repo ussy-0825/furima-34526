737e8556275930acf229a702014e148001a5bc0d
ussy-0825 <uy.advance@gmail.com>
2021-02-15 22:08:36 +0900

    商品出品機能の追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,24 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
+
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :price, :text, :category_id, :status_id, :prefectures_id, :delivery_fee_id, :shipping_id, :image,).merge(user_id: current_user.id)
+  end
+
 end
---
+++ app/models/item.rb
@@ -0,0 +1,21 @@
+class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category, :status, :prefectures, :deliveryfee, :shipping
+  has_one_attached :image
+
+  validates :price, numericality: { greater_than_or_equal_to: 0, 
+                                    less_than_or_equal_to:9999999,
+                                    message: "Out of setting range" }
+
+  with_options presence: true do
+    validates :image, :name, :text, :category_id, :status_id, :prefectures_id, :delivery_fee_id, :shipping_id, :user_id
+    validates :price, format: { with: /\A[0-9]+\z/, message: "Half-width number"}
+  end
+    
+  validates :category_id,     numericality: { other_than: 0, message: "Select" }
+  validates :status_id,       numericality: { other_than: 0, message: "Select" }
+  validates :prefectures_id,  numericality: { other_than: 0, message: "Select" }
+  validates :delivery_fee_id, numericality: { other_than: 0, message: "Select" }
+  validates :shipping_id,     numericality: { other_than: 0, message: "Select" }
+
+end
