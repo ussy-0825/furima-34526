1b5a248fc4b394e30a28475cefd76ffb43b4ea63
ussy-0825 <uy.advance@gmail.com>
2021-02-16 21:35:15 +0900

    itemsコントローラーの修正、編集ページから詳細ページに戻るボタンのパス修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
-  before_action :edit_access_check, only: [:edit]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :edit_access_check, only: [:edit, :update]
+  before_action :set_item, only: [:show, :edit]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -20,11 +21,9 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
@@ -43,13 +42,15 @@ class ItemsController < ApplicationController
                                  :shipping_id, :image).merge(user_id: current_user.id)
   end
 
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+
   def edit_access_check
     item = Item.find(params[:id])
-    if user_signed_in? && current_user.id != item.user_id
+    if current_user.id != item.user_id
       redirect_to root_path
-    elsif user_signed_in?
-    else
-      redirect_to new_user_session_path
     end
   end
 end
