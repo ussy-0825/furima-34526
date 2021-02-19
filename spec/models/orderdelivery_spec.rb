c15b19279ca98b0544404eac53c37e0af46c4402
ussy-0825 <uy.advance@gmail.com>
2021-02-19 12:14:02 +0900

    orderdeliveryモデル(Formオブジェクト)の単体テスト

---
+++ spec/models/orderdelivery_spec.rb
@@ -0,0 +1,63 @@
+require 'rails_helper'
+
+RSpec.describe Orderdelivery, type: :model do
+  before do
+    @orderdelivery = FactoryBot.build(:orderdelivery)
+  end
+
+  describe '商品購入機能' do
+    context '商品の購入がうまくいく時' do
+      it '必要な情報を適切に入力すれば購入できる' do
+        expect(@orderdelivery).to be_valid
+      end
+      it '建物名が入力されていなくても購入できる' do
+       @orderdelivery.building = nil
+       expect(@orderdelivery).to be_valid 
+      end
+    end
+
+    context '商品の購入がうまくいかない時' do
+      it 'クレジットカード情報が入力されていなければ購入できない' do
+        @orderdelivery.token = nil
+        @orderdelivery.valid?
+        expect(@orderdelivery.errors.full_messages).to include("Token can't be blank")
+      end
+      it '郵便番号が入力されていなければ購入できない' do
+        @orderdelivery.postal_code = nil
+        @orderdelivery.valid?
+        expect(@orderdelivery.errors.full_messages).to include("Postal code can't be blank")
+      end
+      it '郵便番号に-(ハイフン)が無ければ購入できない' do
+        @orderdelivery.postal_code = "1111111"
+        @orderdelivery.valid?
+        expect(@orderdelivery.errors.full_messages).to include("Postal code code Input correctly")
+      end
+      it '都道府県が選択されていなければ購入できない' do
+        @orderdelivery.prefectures_id = 0
+        @orderdelivery.valid?
+        expect(@orderdelivery.errors.full_messages).to include("Prefectures Select")
+      end
+      it '市区町村が入力されていなければ購入できない' do
+        @orderdelivery.city = nil
+        @orderdelivery.valid?
+        expect(@orderdelivery.errors.full_messages).to include("City can't be blank")
+      end
+      it '電話番号が入力されていなければ購入できない' do
+        @orderdelivery.phone = nil
+        @orderdelivery.valid?
+        expect(@orderdelivery.errors.full_messages).to include("Phone can't be blank")
+      end
+      it '電話番号が半角数字でなければ購入できない' do
+        @orderdelivery.phone = "０９０１１１１１１１１"
+        @orderdelivery.valid?
+        expect(@orderdelivery.errors.full_messages).to include("Phone number Input only number")
+      end
+      it '電話番号が半角数字でなければ購入できない' do
+        @orderdelivery.phone = "ゼロキューゼロイチイチ"
+        @orderdelivery.valid?
+        expect(@orderdelivery.errors.full_messages).to include("Phone number Input only number")
+      end
+    end
+
+  end
+end
